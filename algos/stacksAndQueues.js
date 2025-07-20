595da243cf0fc3b1fafdf7794b4ad855708f2c3a
Kevin FERNANDO <kevin.fernando@groupe-creative.fr>
2025-07-20 11:49:36 +0200

    reviewing queues

--- algos/stacksAndQueues.js
+++ algos/stacksAndQueues.js
@@ -1,1 +1,25 @@
-export class Queue {}
+export class Queue {
+  constructor() {
+    this.items = []
+  }
+
+  enqueue(element) {
+    this.items.push(element)
+  }
+
+  dequeue() {
+    this.items.shift()
+  }
+
+  peek() {
+    return this.items[0]
+  }
+
+  display(msg = '') {
+    if (msg) {
+      console.log(msg)
+    }
+
+    console.log(this.items)
+  }
+}
--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 import { LinkedList } from './algos/linkedlists.js'
+import { Queue } from './algos/stacksAndQueues.js'
 
+console.log('================LINKDED LISTS================')
 const list = new LinkedList()
 list.insert('hello')
 list.insert('test')
 list.insert(1239438)
 
 list.display()
+console.log('\n')
+
+console.log('================QUEUES AND STACKS================')
+const queue = new Queue()
+
+queue.enqueue('hello')
+queue.enqueue('test')
+queue.display('after enqueueing:')
+
+queue.dequeue()
+queue.dequeue()
+queue.display('after dequeueing:')
+
+// enqueueing 2nd time
+queue.enqueue(1345)
+queue.enqueue('hey man whats up ?')
+queue.enqueue(12.55566322)
+queue.enqueue(false)
+
+queue.display('after enqueueing 2nd time:')
+
+queue.dequeue()
+queue.dequeue()
+
+queue.display('after dequeueing 2nd time:')
